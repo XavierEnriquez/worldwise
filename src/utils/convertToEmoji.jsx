8298ca0438def56cad7e2dc5184ad79dc111fe53
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-14 19:13:07 -0600

    Create convertToEmoji.jsx

---
+++ src/utils/convertToEmoji.jsx
@@ -0,0 +1,11 @@
+/*
+Starting from Windows 10 and forward, windows OS does not support emoji flags in chrome based browsers. Need to implement workaround this. An option is the restcountries.com API
+ */
+
+// export default function convertToEmoji(countryCode) {
+//   const codePoints = countryCode
+//     .toUpperCase()
+//     .split("")
+//     .map((char) => 0x1f1e6 + char.charCodeAt(0) - 65);
+//   return String.fromCodePoint(...codePoints);
+// }
