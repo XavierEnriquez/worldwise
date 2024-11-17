c56daf256e09d3ce0223ae057091031c65131246
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-16 21:14:16 -0600

    Update Form.jsx

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,52 +1,86 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import styles from "./Form.module.css";
-import Button from "../button/Button";
-import ButtonBack from "../button/ButtonBack";
+import { useUrlPosition } from "../../hooks/useUrlPosition";
+import Spinner from "../spinner/Spinner";
+import FormLocIntake from "./FormLocIntake";
+import useCities from "../../contexts/useCities";
+
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 function Form() {
+  const navigate = useNavigate();
+
+  const { createNewCity, isLoading } = useCities();
+
+  const [loadingGeoloc, setLoadingGeoloc] = useState(false);
+  const [lat, lng] = useUrlPosition();
+
   const [cityName, setCityName] = useState("");
-  // const [country, setCountry] = useState("");
+  const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [emoji, setEmoji] = useState("");
+
+  useEffect(
+    function () {
+      async function fetchCityData() {
+        try {
+          setLoadingGeoloc(true);
+          const res = await fetch(
+            `${BASE_URL}?latitude=${lat}&longitude=${lng}`
+          );
+          const data = await res.json();
+
+          setCityName(data.city || data.locality);
+          setCountry(data.countryName);
+          setEmoji(data.countryCode);
+          setNotes("");
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoadingGeoloc(false);
+        }
+      }
+      fetchCityData();
+    },
+    [lat, lng]
+  );
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+
+    await createNewCity(newCity);
+    navigate("/app");
+  }
+
+  if (loadingGeoloc) return <Spinner />;
 
   return (
-    <form className={styles.form}>
-      <div className={styles.row}>
-        <label htmlFor="cityName">City name</label>
-        <input
-          id="cityName"
-          onChange={(e) => setCityName(e.target.value)}
-          value={cityName}
-        />
-        {/* <span className={styles.flag}>{emoji}</span> */}
-      </div>
-
-      <div className={styles.row}>
-        <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
-          id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-        />
-      </div>
-
-      <div className={styles.row}>
-        <label htmlFor="notes">Notes about your trip to {cityName}</label>
-        <textarea
-          id="notes"
-          onChange={(e) => setNotes(e.target.value)}
-          value={notes}
-        />
-      </div>
-
-      <div className={styles.buttons}>
-        <Button type="primary">Add</Button>
-        <ButtonBack />
-      </div>
-    </form>
+    <FormLocIntake
+      isLoading={isLoading}
+      cityName={cityName}
+      emoji={emoji}
+      date={date}
+      notes={notes}
+      onSetCityName={setCityName}
+      onSetDate={setDate}
+      onSetNotes={setNotes}
+      onHandleSubmit={handleSubmit}
+    />
   );
 }
 
