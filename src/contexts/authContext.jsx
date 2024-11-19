35f58363bdf6c4785d16cf8d61c3d4be4f339e8d
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-18 22:04:02 -0600

    Update authContext.jsx

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,48 +1,5 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext } from "react";
 
 const AuthContext = createContext();
 
-const authInitialState = {
-  user: null,
-  isAuthenticated: false,
-};
-
-function authReducer(state, action) {
-  switch (action.type) {
-    case "login":
-      return { ...state, user: action.payload, isAuthenticated: true };
-
-    case "logout":
-      return { ...state, user: null, isAuthenticated: false };
-
-    default:
-      throw new Error("Unknown action");
-  }
-}
-
-function AuthProvider({ children }) {
-  const [{ user, isAuthenticated, dispatch }] = useReducer(
-    authReducer,
-    authInitialState
-  );
-
-  function login(email, password) {}
-
-  function logout() {
-    dispatch({ type: "logout" });
-  }
-
-  return (
-    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
-
-export default AuthProvider;
-
-function useAuth() {
-  const context = useContext(AuthContext);
-  if (context === undefined)
-    throw new Error("AuthContext is used outside AuthProvider");
-}
+export default AuthContext;
